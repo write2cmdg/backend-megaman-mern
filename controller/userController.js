cd90502165f3aa36a55f0fd14e9fde7f9ab5e8c9
write2cmdg <write2cmdg@gmail.com>
2023-05-11 18:19:03 -0400

    added jwt

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,11 @@
 
 const User = require('../models/userModel')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
+}
 
 
 //Login user
@@ -17,7 +22,12 @@ const registerUser = async (req, res) => {
 
     try {
         const user = await User.register(email, password)
-        res.status(200).json({ email, user })
+
+        //create token
+        const token = createToken(user._id)
+
+        res.status(200).json({ email, token })
+
     } catch (error) {
         res.status(400).json({error: error.message})
     }
