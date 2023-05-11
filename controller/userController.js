5debbe48b78d0fa5ce816ab71e47d72ef710f941
write2cmdg <write2cmdg@gmail.com>
2023-05-11 17:21:43 -0400

    created static register method in user model

--- controller/userController.js
+++ controller/userController.js
@@ -6,29 +6,14 @@ const mongoose = require('mongoose')
 
 //Login user
 const loginUser = async (req, res) => {
-    
-    const users = await User.find({}).sort({ createdAt: -1 })
-    res.status(200).json(users)
+    res.json({mssg: 'login user'})
+
 }
 
 //Register user
 const registerUser = async (req, res) => {
+    res.json({mssg: 'register user'})
 
-    const { id } = req.params 
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-
-        return res.status(404).json({ error: 'user not found'})
-    }
-
-    const user = await User.findById(id)
-
-    if (!user) {
-
-        return res.status(404).json({ error: `user: ${id}, not found` })
-    }
-
-    res.status(200).json(user)
 
 }
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
 
@@ -16,6 +17,25 @@ const userSchema = new Schema({
     }
 },{ timestamps: true }
 )
+
+//static register method
+userSchema.statics.register = async (email, password) => {
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = await this.create({ email, password: hash })
+
+    return user
+
+}
+
 
 const userModel = mongoose.model("User", userSchema);
 
