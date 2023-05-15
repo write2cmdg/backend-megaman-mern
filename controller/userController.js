55cf0ae1bcbe611843802a2d23ad36864ef64260
write2cmdg <write2cmdg@gmail.com>
2023-05-15 01:35:39 -0400

    updated token expire to 1 day

--- controller/userController.js
+++ controller/userController.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d'})
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d'})
 }
 
 
